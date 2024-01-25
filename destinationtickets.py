efca48daffc99bf4f4e57f0f4fd3dcef4b7d7a3f
Peter Clark <peterjosephclark1@gmail.com>
2024-01-25 08:49:33 -0900

    3D visualization successful with multiple edges in between nodes.

--- destinationtickets.py
+++ destinationtickets.py
@@ -2,7 +2,13 @@ import os
 import csv
 import numpy
 import heapq
+import networkx as nx
 import functions
+import numpy as np
+from mpl_toolkits.mplot3d.art3d import Line3DCollection
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 ## get data ##
 wm = []
@@ -43,7 +49,7 @@ max_len = 16
 max_ships = max_len
 max_trains = max_len
 
-map_routes = True
+map_routes = False
 if map_routes:
     sorted_routes = worldmap.get_sorted_paths(start, end, max_len, max_ships, max_trains)
     print(f"Shortest Route:")
@@ -52,6 +58,115 @@ if map_routes:
     functions.print_sorted_paths(sorted_routes)
 
 
+## Graph building and viz
+world_map_graph = nx.MultiGraph()
+world_map_graph.clear()
+_2d = False
+_3d = True
+
+## 2D
+# add city nodes
+if _2d==True:
+    for id, city in functions.city_w.items():
+        world_map_graph.add_node(id, name=city['name'], has_harbor=city['harbor'], can_harbor=city['can_harbor'])
+
+    # add routes
+    for edge in functions.tickets_w:
+        city1, city2, weight, _type, color = edge
+        world_map_graph.add_edge(city1, city2, weight=weight, _type=_type, color=functions.colors[color])
+    # get pos from viz
+    pos = nx.planar_layout(world_map_graph)
+    # draw nodes
+    nx.draw_networkx_nodes(world_map_graph, pos, node_size=200)
+    # draw edges
+    for edge in world_map_graph.edges(data=True): # split into a, b, data['x': 'y', ...]
+        nx.draw_networkx_edges(world_map_graph, pos, edgelist=[(edge[0],edge[1])], edge_color=edge[2]['color'])
+
+    nx.draw_networkx_labels(world_map_graph, pos)
+    plt.show()
+
+
+### 3D
+# plot
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+
+
+# add cities
+for id, city in functions.city_w.items():
+    world_map_graph.add_node(id, name=city['name'], has_harbor=city['harbor'], can_harbor=city['can_harbor'])
+
+# add routes
+for edge in functions.tickets_w:
+    city1, city2, weight, _type, color = edge
+    world_map_graph.add_edge(city1, city2, weight=weight, _type=_type, color=functions.colors[color])
+
+# get pos from viz
+pos = nx.spring_layout(world_map_graph, dim=3, seed=70)
+
+# rescale length to reflect weight
+""" for edge in world_map_graph.edges(data=True):
+    weight = edge[2]['weight']
+    scale_factor = weight / 2  # Adjust the scaling factor as needed
+
+    # Rescale node positions
+    pos[edge[0]] = [pos[edge[0]][0] * scale_factor, pos[edge[0]][1] * scale_factor, pos[edge[0]][2]]
+    pos[edge[1]] = [pos[edge[1]][0] * scale_factor, pos[edge[1]][1] * scale_factor, pos[edge[1]][2]]
+ """
+
+#nodes gotten from layout
+for node, (x, y, z) in pos.items():
+    ax.scatter(x, y, z, c='b', marker='x', label=node, sizes=[8])
+    ax.text(x, y, z, node, fontsize=8, color='black')
+
+print(world_map_graph.edges(data=True))
+# Draw edges
+double_edges=set()
+for edge in world_map_graph.edges(data=True):
+    """ ax.plot([pos[edge[0]][0], pos[edge[1]][0]],
+            [pos[edge[0]][1], pos[edge[1]][1]],
+            [pos[edge[0]][2], pos[edge[1]][2]], c=edge[2]['color'], alpha=0.7, linewidth=4 if edge[2]['color']=='darkslategray' else 2)
+
+    weight = edge[2]['weight']
+    num_points = 200  # Adjust the number of points for smoother curves
+    t = np.linspace(0, 1, num_points)
+    points = np.array([
+        np.linspace(pos[edge[0]][0], pos[edge[1]][0], num_points),
+        np.linspace(pos[edge[0]][1], pos[edge[1]][1], num_points),
+        np.linspace(pos[edge[0]][2], pos[edge[1]][2], num_points),
+    ]).T
+    curves = Line3DCollection([points], colors=edge[2]['color'], alpha=0.5)
+    ax.add_collection3d(curves)"""
+
+    weight = edge[2]['weight']
+    num_points = 200
+    t = np.linspace(0, 1, num_points)
+
+    p0 = np.array([pos[edge[0]][0], pos[edge[0]][1], pos[edge[0]][2]])
+    p2 = np.array([pos[edge[1]][0], pos[edge[1]][1], pos[edge[1]][2]])
+    p1 = (p0 + p2) / 2
+    if(edge[0],edge[1]) in double_edges or (edge[1],edge[0]) in double_edges:
+        # Midpoint as control point with an offset in the x-direction
+        offset_amount = np.random.uniform(-0.10,0.10,size=3)  # You can adjust this value
+        p1 = (p0 + p2) / 2 + np.array(offset_amount)
+    else:
+        double_edges.add((edge[0],edge[1]))
+        double_edges.add((edge[1],edge[0]))   
+
+    # Define the Bezier curve for each coordinate
+    x_curve = functions.bezier_curve(p0[0], p1[0], p2[0], t)
+    y_curve = functions.bezier_curve(p0[1], p1[1], p2[1], t)
+    z_curve = functions.bezier_curve(p0[2], p1[2], p2[2], t)
+    curves = Line3DCollection([list(zip(x_curve, y_curve, z_curve))], colors=edge[2]['color'], alpha=0.5)    
+    ax.add_collection3d(curves)
+
+background_color = 'white'
+ax.xaxis.set_pane_color(mpl.colors.to_rgba(background_color))
+ax.yaxis.set_pane_color(mpl.colors.to_rgba(background_color))
+ax.zaxis.set_pane_color(mpl.colors.to_rgba(background_color))
+
+plt.show()
 
 ########################################################################
 ##########################    GREAT LAKES   ############################
--- functions.py
+++ functions.py
@@ -130,58 +130,209 @@ class DestinationTickets:
 
 
 ## dictionary definitions ##
-#TODO: Harbor-ability
-city_w = {
-    'AAO': 'Antarctic Ocean',
-    'ALQ': 'Al-Qahira',
-    'ANC': 'Anchorage',
-    'ATH': 'Athina',
-    'BKK': 'Bangkok',
-    'BUE': 'Buenos Aires',
-    'CAR': 'Caracas',
-    'CAS': 'Casablanca',
-    'CMB': 'Cambridge Bay',
-    'CPT': 'Cape Town',
-    'CHR': 'Christchurch',
-    'DES': 'Dar Es Salaam',
-    'DJI': 'Djibouti',
-    'EDI': 'Edinburgh',
-    'HAM': 'Hamburg',
-    'HKG': 'Hong Kong',
-    'HON': 'Honolulu',
-    'JAK': 'Jakarta',
-    'LAH': 'Lahore',
-    'LAG': 'Lagos',
-    'LIM': 'Lima',
-    'LOS': 'Los Angeles',
-    'LUA': 'Luanda',
-    'MAN': 'Manila',
-    'MAR': 'Marseille',
-    'MEX': 'Mexico',
-    'MIA': 'Miami',
-    'MOS': 'Moskva',
-    'MUM': 'Mumbai',
-    'NVS': 'Novosibirsk',
-    'NYC': 'New York',
-    'PER': 'Perth',
-    'PET': 'Petropavlovsk',
-    'PTM': 'Port Moresby',
-    'PTP': 'Toamasina',
-    'REY': 'Reykjavik',
-    'RIO': 'Rio De Janeiro',
-    'SYD': 'Sydney',
-    'TOA': 'Toamasina',
-    'TEH': 'Tehran',
-    'TIK': 'Tiksi',
-    'TOK': 'Tokyo',
-    'VAL': 'Valparaiso',
-    'YAK': 'Yakutsk',
-    'VAN': 'Vancouver',
-    'WIN': 'Winnipeg'
+type = {1:'land', 2:'sea'}
+
+colors = {# some changed for visibility
+    'g':'green', 'p':'mediumvioletred', # heliotrope really
+    'w':'indigo', 'r':'red', 'y':'yellow',
+    'b':'black', 'any':'sienna', 'db':"darkslategray"
 }
 
+city_w = {
+    'AAO': {'name': 'Antarctic Ocean', 'harbor': False, 'can_harbor': False},
+    'ALQ': {'name': 'Al-Qahira', 'harbor': False, 'can_harbor': True},
+    'ANC': {'name': 'Anchorage', 'harbor': False, 'can_harbor': True},
+    'ATH': {'name': 'Athina', 'harbor': False, 'can_harbor': True},
+    'BKK': {'name': 'Bangkok', 'harbor': False, 'can_harbor': True},
+    'BUE': {'name': 'Buenos Aires', 'harbor': False, 'can_harbor': True},
+    'CAR': {'name': 'Caracas', 'harbor': False, 'can_harbor': True},
+    'CAS': {'name': 'Casablanca', 'harbor': False, 'can_harbor': True},
+    'CMB': {'name': 'Cambridge Bay', 'harbor': False, 'can_harbor': True},
+    'CPT': {'name': 'Cape Town', 'harbor': False, 'can_harbor': True},
+    'CHR': {'name': 'Christchurch', 'harbor': False, 'can_harbor': True},
+    'DES': {'name': 'Dar Es Salaam', 'harbor': False, 'can_harbor': True},
+    'DJI': {'name': 'Djibouti', 'harbor': False, 'can_harbor': True},
+    'EDI': {'name': 'Edinburgh', 'harbor': False, 'can_harbor': True},
+    'HAM': {'name': 'Hamburg', 'harbor': False, 'can_harbor': True},
+    'HKG': {'name': 'Hong Kong', 'harbor': False, 'can_harbor': True},
+    'HON': {'name': 'Honolulu', 'harbor': False, 'can_harbor': True},
+    'JAK': {'name': 'Jakarta', 'harbor': False, 'can_harbor': True},
+    'LAH': {'name': 'Lahore', 'harbor': False, 'can_harbor': True},
+    'LAG': {'name': 'Lagos', 'harbor': False, 'can_harbor': True},
+    'LIM': {'name': 'Lima', 'harbor': False, 'can_harbor': True},
+    'LOS': {'name': 'Los Angeles', 'harbor': False, 'can_harbor': True},
+    'LUA': {'name': 'Luanda', 'harbor': False, 'can_harbor': True},
+    'MAN': {'name': 'Manila', 'harbor': False, 'can_harbor': True},
+    'MAR': {'name': 'Marseille', 'harbor': False, 'can_harbor': True},
+    'MEX': {'name': 'Mexico', 'harbor': False, 'can_harbor': True},
+    'MIA': {'name': 'Miami', 'harbor': False, 'can_harbor': True},
+    'MOS': {'name': 'Moskva', 'harbor': False, 'can_harbor': True},
+    'MUM': {'name': 'Mumbai', 'harbor': False, 'can_harbor': True},
+    'NOV': {'name': 'Novosibirsk', 'harbor': False, 'can_harbor': True},
+    'NYC': {'name': 'New York', 'harbor': False, 'can_harbor': True},
+    'PER': {'name': 'Perth', 'harbor': False, 'can_harbor': True},
+    'PET': {'name': 'Petropavlovsk', 'harbor': False, 'can_harbor': True},
+    'PTM': {'name': 'Port Moresby', 'harbor': False, 'can_harbor': True},
+    'TOA': {'name': 'Toamasina', 'harbor': False, 'can_harbor': True},
+    'REY': {'name': 'Reykjavik', 'harbor': False, 'can_harbor': True},
+    'RIO': {'name': 'Rio De Janeiro', 'harbor': False, 'can_harbor': True},
+    'SYD': {'name': 'Sydney', 'harbor': False, 'can_harbor': True},
+    'TOA': {'name': 'Toamasina', 'harbor': False, 'can_harbor': True},
+    'TEH': {'name': 'Tehran', 'harbor': False, 'can_harbor': True},
+    'TIK': {'name': 'Tiksi', 'harbor': False, 'can_harbor': True},
+    'TOK': {'name': 'Tokyo', 'harbor': False, 'can_harbor': True},
+    'VAL': {'name': 'Valparaiso', 'harbor': False, 'can_harbor': True},
+    'YAK': {'name': 'Yakutsk', 'harbor': False, 'can_harbor': True},
+    'VAN': {'name': 'Vancouver', 'harbor': False, 'can_harbor': True},
+    'WIN': {'name': 'Winnipeg', 'harbor': False, 'can_harbor': True}
+}
+
+tickets_w = [# NA
+    ('ANC', 'VAN', 4, 1, 'db'),  
+    ('VAN', 'LOS', 1, 1, 'g'),
+    ('VAN', 'LOS', 1, 1, 'r'),
+    ('VAN', 'WIN', 2, 1, 'y'),
+    ('LOS', 'MEX', 2, 1, 'w'),
+    ('LOS', 'MEX', 2, 1, 'y'),
+    ('LOS', 'NYC', 4, 1, 'p'),
+    ('LOS', 'NYC', 4, 1, 'b'),
+    ('LOS', 'WIN', 3, 1, 'any'),  # any
+    ('WIN', 'NYC', 2, 1, 'g'),
+    ('WIN', 'CMB', 4, 1, 'b'),
+    ('MIA', 'NYC', 2, 1, 'w'),
+    ('MEX', 'CAR', 3, 1, 'r'),
+    ('MEX', 'CAR', 3, 1, 'p'),
+    ('CAR', 'LIM', 2, 1, 'y'),  
+    # SA
+    ('CAR', 'LIM', 2, 1, 'w'),
+    ('CAR', 'RIO', 4, 1, 'b'),
+    ('CAR', 'RIO', 4, 1, 'g'),
+    ('LIM', 'VAL', 2, 1, 'any'),  # any
+    ('LIM', 'VAL', 2, 1, 'any'),  # any
+    ('BUE', 'RIO', 1, 1, 'r'),
+    ('BUE', 'RIO', 1, 1, 'w'),
+    ('MAR', 'CAS', 2, 1, 'db'),  
+    # EUR
+    ('MAR', 'HAM', 1, 1, 'p'),
+    ('MAR', 'HAM', 1, 1, 'r'),
+    ('HAM', 'ATH', 2, 1, 'g'),
+    ('HAM', 'MOS', 2, 1, 'b'),
+    ('HAM', 'MOS', 2, 1, 'w'),
+    ('MUR', 'MOS', 2, 1, 'p'),
+    ('ATH', 'TEH', 2, 1, 'any'),  # any
+    ('MOS', 'TEH', 3, 1, 'r'),
+    ('MOS', 'NOV', 4, 1, 'g'),
+    ('MOS', 'NOV', 4, 1, 'y'),
+    # AFR
+    ('CAS', 'LAG', 4, 1, 'any'),  # any
+    ('CAS', 'ALQ', 3, 1, 'any'),  # any
+    ('ALQ', 'DJI', 2, 1, 'w'),
+    ('ALQ', 'DJI', 2, 1, 'r'),
+    ('ALQ', 'TEH', 1, 1, 'b'),
+    ('ALQ', 'TEH', 1, 1, 'y'),
+    ('LAG', 'LUA', 1, 1, 'p'),
+    ('LAG', 'LUA', 1, 1, 'y'),
+    ('LUA', 'DES', 4, 1, 'db'),  # db
+    ('CPT', 'LUA', 2, 1, 'any'),  # any
+    ('CPT', 'DES', 3, 1, 'g'),
+    ('CPT', 'DES', 3, 1, 'p'),
+    ('DES', 'DJI', 1, 1, 'b'),
+    ('DES', 'DJI', 1, 1, 'r'),
+    # ASIA
+    ('TEH', 'LAH', 4, 1, 'db'),  # db
+    ('TEH', 'MUM', 3, 1, 'w'),
+    ('TEH', 'MUM', 3, 1, 'p'),
+    ('NOV', 'TIK', 3, 1, 'any'),  # any
+    ('NOV', 'YAK', 3, 1, 'p'),
+    ('NOV', 'BEI', 3, 1, 'r'),
+    ('NOV', 'BEI', 3, 1, 'b'),
+    ('NOV', 'LAH', 2, 1, 'w'),
+    ('TIK', 'YAK', 1, 1, 'g'),
+    ('LAH', 'BEI', 6, 1, 'db'),  # db
+    ('LAH', 'MUM', 1, 1, 'g'),
+    ('LAH', 'MUM', 1, 1, 'b'),
+    ('MUM', 'BKK', 3, 1, 'r'),
+    ('MUM', 'BKK', 3, 1, 'y'),
+    ('YAK', 'PET', 3, 1, 'w'),
+    ('YAK', 'BEI', 3, 1, 'y'),
+    ('HKG', 'BEI', 2, 1, 'w'),
+    ('HKG', 'BEI', 2, 1, 'g'),
+    ('BKK', 'HKG', 1, 1, 'p'),
+    ('BKK', 'HKG', 1, 1, 'b'),
+    # OCE
+    ('PER', 'SYD', 2, 1, 'y'),  
+    ('PER', 'SYD', 2, 1, 'w'),
+    ('DAR', 'PER', 2, 1, 'r'),
+    ('SYD', 'DAR', 2, 1, 'g'),
+    # (Sea Connections)
+    # NA
+    ('ANC', 'CMB', 6, 2, 'b'),  
+    ('CMB', 'REY', 6, 2, 'w'),
+    ('NYC', 'REY', 6, 2, 'y'),
+    ('NYC', 'EDI', 7, 2, 'p'),
+    ('NYC', 'EDI', 7, 2, 'r'),
+    ('MIA', 'CAS', 7, 2, 'g'),
+    ('MIA', 'CAR', 2, 2, 'w'),
+    # SA
+    ('CAR', 'LAG', 7, 2, 'r'),
+    ('RIO', 'LUA', 6, 2, 'any'),  # any
+    ('RIO', 'CPT', 6, 2, 'w'),
+    ('RIO', 'CPT', 6, 2, 'b'),
+    ('BUE', 'CPT', 6, 2, 'p'),
+    ('BUE', 'CPT', 6, 2, 'y'),
+    ('VAL', 'BUE', 3, 2, 'g'),
+    # EUR
+    ('REY', 'MUR', 4, 2, 'g'),
+    ('REY', 'EDI', 2, 2, 'any'),  # any
+    ('EDI', 'HAM', 1, 2, 'y'),
+    ('EDI', 'HAM', 1, 2, 'b'),
+    ('EDI', 'MAR', 1, 2, 'w'),
+    ('EDI', 'MAR', 1, 2, 'g'),
+    ('MUR', 'TIK', 7, 2, 'r'),
+    ('MAR', 'ATH', 2, 2, 'r'),
+    ('ATH', 'ALQ', 1, 2, 'g'),
+    # AFR + AAO
+    ('DES', 'MUM', 4, 2, 'w'),
+    ('DES', 'JAK', 7, 2, 'p'),
+    ('DES', 'JAK', 7, 2, 'g'),
+    ('DES', 'TOA', 1, 2, 'y'),
+    ('CPT', 'TOA', 3, 2, 'any'),  # any
+    ('CPT', 'AAO', 5, 2, 'g'),
+    ('CPT', 'AAO', 5, 2, 'r'),
+    ('AAO', 'PER', 5, 2, 'w'),
+    ('AAO', 'PER', 5, 2, 'p'),
+    # ASIA
+    ('TIK', 'ANC', 8, 2, 'y'),
+    ('TIK', 'PET', 7, 2, 'b'),
+    ('PET', 'ANC', 3, 2, 'p'),
+    ('PET', 'TOK', 2, 2, 'any'),  # any
+    ('HKG', 'TOK', 3, 2, 'any'),  # any
+    ('HKG', 'MAN', 1, 2, 'p'),
+    ('BKK', 'JAK', 2, 2, 'r'),
+    ('BKK', 'MAN', 2, 2, 'w'),
+    ('JAK', 'PER', 3, 2, 'any'),  # any
+    ('JAK', 'DAR', 2, 2, 'b'),
+    ('JAK', 'MAN', 2, 2, 'any'),  # any
+    # OCE
+    ('MAN', 'TOK', 2, 2, 'y'),
+    ('MAN', 'HON', 5, 2, 'w'),
+    ('TOK', 'VAN', 6, 2, 'w'),
+    ('TOK', 'LOS', 7, 2, 'b'),
+    ('TOK', 'LOS', 7, 2, 'g'),
+    ('TOK', 'HON', 5, 2, 'r'),
+    ('DAR', 'PTM', 1, 2, 'r'),
+    ('PTM', 'HON', 3, 2, 'g'),
+    ('SYD', 'PTM', 3, 2, 'y'),
+    ('SYD', 'LIM', 8, 2, 'p'),
+    ('SYD', 'LIM', 8, 2, 'b'),
+    ('SYD', 'CHR', 1, 2, 'r'),
+    ('SYD', 'CHR', 1, 2, 'w'),
+    ('CHR', 'VAL', 7, 2, 'y'),
+    ('HON', 'LOS', 3, 2, 'y')]
+
+
 city_gl = {
-    'BAY': 'Bay City',
+    'BAY': {'name': 'Bay City', 'harbor': False, 'can_harbor':True},
     'SCR': 'Scranton',
     'BUF': 'Buffalo',
     'ALB': 'Albany',
@@ -215,14 +366,6 @@ city_gl = {
     'SUD': 'Sudbury',
     'THU': 'Thunder Bay',
     'SBM': 'South Baymouth'
-}
-
-type = {1:'land', 2:'sea'}
-
-colors = {
-    'g':'green', 'p':'pink', # heliotrope really
-    'w':'white', 'r':'red', 'y':'yellow',
-    'b':'black', 'any':'any', 'db':"double"
 }
 
 def initialize_world():
@@ -420,7 +563,7 @@ def initialize_world_tickets():
     tix.add_ticket(DestinationTicket('MOS', 'MIA', 13))
     tix.add_ticket(DestinationTicket('HKG', 'MOS', 13))
     tix.add_ticket(DestinationTicket('EDI', 'VAN', 13))
-    tix.add_ticket(DestinationTicket('DRW', 'NVS', 13))
+    tix.add_ticket(DestinationTicket('DRW', 'NOV', 13))
     tix.add_ticket(DestinationTicket('MUM', 'REY', 13))
     tix.add_ticket(DestinationTicket('BEI', 'HAM', 13))
     tix.add_ticket(DestinationTicket('BEI', 'MEX', 13))
@@ -492,3 +635,6 @@ def print_iteration(i):
 def nothing():
     i=0
     i+1
+
+def bezier_curve(p0, p1, p2, t):
+    return (1 - t)**2 * p0 + 2 * (1 - t) * t * p1 + t**2 * p2
